4c9f38f2ef277fc5fe020381e6b98faff77b3c03
Michael Granados <michaelgranados@gmail.com>
2013-03-22 11:10:35 -0300

    Creating a first build

---
+++ build/source/navframe-js.js
@@ -0,0 +1,7 @@
+var TREE_ITEMS = [
+['TOP', './index.html', ['Apolo', 'Apolo/index.html', ['Core', 'Apolo/Core/index.html', ['Apolo.php', 'Apolo/Core/Apolo.php.source.html']
+, ['Route.php', 'Apolo/Core/Route.php.source.html']
+]
+]
+]
+];
